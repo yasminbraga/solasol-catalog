797bc7af8694c53b35e6fc7b41ec5f6b3438eb12
yasminbraga <yasminbragateixeira@gmail.com>
2021-07-21 17:49:53 -0300

    adiciona update categories

--- app/Controllers/Http/CategoriesController.ts
+++ app/Controllers/Http/CategoriesController.ts
@@ -1,8 +1,13 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema } from '@ioc:Adonis/Core/Validator'
 
 import Category from 'App/Models/Category'
 
 export default class CategoriesController {
+  protected _validationMessages = {
+    required: 'Preencha este campo',
+  }
+
   public async index({ view }: HttpContextContract) {
     try {
       const categories = await Category.query()
@@ -16,23 +21,58 @@ export default class CategoriesController {
     return view.render('categories/create')
   }
 
-  public async store({ request, response }: HttpContextContract) {
-    const { name } = request.only(['name'])
+  public async store({ logger, request, response }: HttpContextContract) {
+    const validationSchema = schema.create({
+      name: schema.string({}),
+    })
+
+    const categoryData = await request.validate({
+      schema: validationSchema,
+      messages: this._validationMessages,
+    })
 
     try {
-      await Category.create({ name })
-      return response.redirect().toRoute('categories/index')
+      await Category.create(categoryData)
+      return response.redirect().toRoute('categories.index')
     } catch (error) {
-      console.error(error)
+      logger.error(error)
       return response.redirect().back()
     }
   }
 
   public async show({}: HttpContextContract) {}
 
-  public async edit({}: HttpContextContract) {}
+  public async edit({ logger, response, request, view }: HttpContextContract) {
+    const id = request.param('id')
+    try {
+      const category = await Category.findOrFail(id)
+      return view.render('categories/edit', { category })
+    } catch (error) {
+      logger.error(error)
+      return response.redirect().back()
+    }
+  }
 
-  public async update({}: HttpContextContract) {}
+  public async update({ request, response, logger }: HttpContextContract) {
+    const id = request.param('id')
+
+    const validationSchema = schema.create({
+      name: schema.string({}),
+    })
+
+    const categoryData = await request.validate({
+      schema: validationSchema,
+      messages: this._validationMessages,
+    })
+
+    try {
+      await Category.query().where({ id }).update(categoryData)
+      return response.redirect().toRoute('categories/index')
+    } catch (error) {
+      logger.error(error)
+      return response.redirect().back()
+    }
+  }
 
   public async destroy({}: HttpContextContract) {}
 }
